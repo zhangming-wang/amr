fec1193bf1a3edb5b58f2681d8467bbc27fe6399
zmwang <zmwang@visionnav.com>
2026-03-05 12:17:14 +0800

    修复删错settings.h中的雷达频率参数

--- common/settings.h
+++ common/settings.h
@@ -5,6 +5,7 @@ static constexpr const char *wifi_password = "403123456";
 static constexpr const char *wifi_IP = "192.168.1.104";
 
 static constexpr const int micro_ros_port = 8888;
+static constexpr const int ydlidar_baudrate = 115200;
 
 static constexpr const char *pc_camera_node_name = "pc_camera_node";
 static constexpr const char *pc_camera_node_namespace = "pc";
